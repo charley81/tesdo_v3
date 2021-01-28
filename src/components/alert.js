78eaa836117e16e5582440f33010549e6f9c8acc
ChrisKnowsDev <chrisharley81@gmail.com>
2021-01-28 15:20:30 -0500

    hard coded ui

---
+++ src/components/alert.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alert = () => {
+  return (
+    <div>
+      <p>alert component</p>
+    </div>
+  )
+}
+
+export default Alert
--- src/components/form.js
+++ src/components/form.js
@@ -1,9 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react'
 import React from 'react'
+import Alert from './alert'
+import List from './list'
 
 const Form = () => {
   return (
-    <div>
-      <h3>form component</h3>
+    <div
+      css={css`
+        max-width: var(--maxWidth);
+        margin: 5vh auto;
+
+        input {
+          display: inline;
+        }
+
+        button {
+          display: inline;
+        }
+      `}
+    >
+      <Alert />
+      <List />
+      <div className="add-container">
+        <input type="text" placeholder="add item" />
+        <button>submit</button>
+      </div>
     </div>
   )
 }
